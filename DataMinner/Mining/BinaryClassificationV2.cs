de58fc388ab0b0cdd00e7f8b66104bf29139a0a2
Brandon G <brandongob@gmail.com>
2019-03-31 13:33:11 -0600

    started implemting binary classification generally

---
+++ DataMinner/Mining/BinaryClassificationV2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.DataView;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMinner.Mining
+{
+    public class BinaryClassificationV2<TRowModel> where TRowModel : class
+    {
+        private readonly MLContext _mlContext;
+        private IDataView _dataView;
+
+        public BinaryClassificationV2(IEnumerable<TRowModel> rows)
+        {
+            _mlContext = new MLContext();
+            _dataView = _mlContext.Data.LoadFromEnumerable<TRowModel>(rows);
+        }
+        
+        public void BuildModel()
+        {
+            _mlContext.BinaryClassification.TrainTestSplit(_dataView, testFraction: 0.2);
+
+        }
+    }
+}
